92c213db308b2f58fe51a833b1bef8a960d87cb1
bxavace <b_xavace@outlook.com>
2022-07-14 16:07:42 +0800

    anime.js feature

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var toggleTheme = document.getElementById("toggle-themeid");
 
 toggleTheme.onclick = () => {
   toggleTheme.classList.toggle("on");
-
   if (toggleTheme.classList.contains("on")) {
     document.body.classList.add("dark");
   } else {
@@ -12,9 +11,21 @@ toggleTheme.onclick = () => {
 
 setTimeout(() => {
   const name = document.querySelector(".name");
-  const namejp = document.querySelector(".name-jp");
+  const namejp = document.querySelector("#name-jp");
   name.style.opacity = 1;
-  namejp.style.opacity = 1;
+  // namejp.style.opacity = 1;
   name.style.filter = "blur(0px)";
-  namejp.style.filter = "blur(0px)";
+  // namejp.style.filter = "blur(0px)";
 }, 1000);
+
+anime({
+  targets: "#name-jp path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutCubic",
+  duration: 3000,
+  delay: function (el, i) {
+    return i * 250;
+  },
+  direction: "alternate",
+  loop: true,
+});
